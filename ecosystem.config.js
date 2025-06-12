09d7639c673e3d1efbed4611ea0cabaceba80f1e
iwaag <>
2025-06-12 16:54:09 +0900

    server configs

---
+++ ecosystem.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  apps: [
+    {
+      name: 'music-caption',
+      script: 'bash',
+      args: [
+        '-c',
+        `
+        source /opt/anaconda3/etc/profile.d/conda.sh &&
+        cd ./music-caption &&
+        if [ ! -f "./env/conda-meta/history" ]; then
+          conda create --yes --prefix ./env python=3.11
+        fi &&
+        conda activate ./env &&
+        pip install poetry &&
+        poetry config virtualenvs.create false &&
+        poetry install &&
+        pip install -r requirements.txt &&
+        uvicorn main:app --host 0.0.0.0 --port 8102
+        `
+      ]
+    }
+    // {
+    //   name :'music-analysis',
+    //   script: 'bash',
+    //   args: '-c "source /opt/anaconda3/etc/profile.d/conda.sh && cd ./MusicAnalyzer && conda activate musicanlys && uvicorn main:app --host 0.0.0.0 --port 8102"',
+    // },
+    // {
+    //   name :'task-manager',
+    //   script: 'bash',
+    //   args: '-c "source /opt/anaconda3/etc/profile.d/conda.sh && cd ./AGDevTaskManager && conda activate AGDevTaskManager && uvicorn main:app --host 0.0.0.0 --port 8001"',
+    // },
+    // {
+    //   name :'task-monitor',
+    //   script: 'bash',
+    //   args: '-c "cd ./agdevtaskmonitor && npm run dev -- --port 8002"',
+    // }
+  ]
+}
